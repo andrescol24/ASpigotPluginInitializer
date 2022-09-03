b5692fd108561b861e67d7cdd03e7ee0c01b317f
BuildTools <unconfigured@null.spigotmc.org>
2022-09-03 17:33:46 -0500

    fixed configuration initialization

--- src/main/java/co/andrescol/mc/library/configuration/AConfigurationObject.java
+++ src/main/java/co/andrescol/mc/library/configuration/AConfigurationObject.java
@@ -4,6 +4,7 @@ import co.andrescol.mc.library.plugin.APlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 public abstract class AConfigurationObject {
@@ -14,20 +15,37 @@ public abstract class AConfigurationObject {
             try {
                 AConfigurationKey key = field.getAnnotation(AConfigurationKey.class);
                 if (key == null) continue;
-                if (field.getType().equals(String.class)) {
-                    field.set(null, configuration.getString(key.value()));
-                } else if (field.getType().equals(Integer.class)) {
-                    field.set(null, configuration.getInt(key.value()));
-                } else if (field.getType().equals(Long.class)) {
-                    field.set(null, configuration.getLong(key.value()));
-                } else if (field.getType().equals(List.class) && field.getGenericType().equals(String.class)) {
-                    field.set(null, configuration.getStringList(key.value()));
-                } else if (field.getType().equals(List.class) && field.getGenericType().equals(Integer.class)) {
-                    field.set(null, configuration.getIntegerList(key.value()));
-                } else if (field.getType().equals(Boolean.class)) {
-                    field.set(null, configuration.getBoolean(key.value()));
+                field.setAccessible(true);
+                Class<?> type = field.getType();
+                // String
+                if (String.class.equals(type)) {
+                    field.set(this, configuration.getString(key.value()));
+                }
+                // Integer
+                else if (Integer.class.equals(type) || int.class.equals(type)) {
+                    field.set(this, configuration.getInt(key.value()));
+                }
+                // Long
+                else if (Long.class.equals(type) || long.class.equals(type)) {
+                    field.set(this, configuration.getLong(key.value()));
+                }
+                // Boolean
+                else if (Boolean.class.equals(type) || boolean.class.equals(type)) {
+                    field.setBoolean(this, configuration.getBoolean(key.value()));
+                }
+                // Lists
+                else if (field.getType().equals(List.class)) {
+                    Class<?> genericType = (Class<?>) ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0];
+                    // String list
+                    if (String.class.equals(genericType)) {
+                        field.set(this, configuration.getStringList(key.value()));
+                    }
+                    // Integer list
+                    else if (Integer.class.equals(genericType)) {
+                        field.set(this, configuration.getIntegerList(key.value()));
+                    }
                 } else {
-                    APlugin.getInstance().warn("Unsupported field type {} for {} config variable", field.getType(), field.getName());
+                    APlugin.getInstance().warn("Unsupported field type {}<{}> for {} config variable", field.getType(), field.getGenericType(), field.getName());
                 }
             } catch (Exception e) {
                 APlugin.getInstance().error("Error setting the value for {}", e, field.getName());
